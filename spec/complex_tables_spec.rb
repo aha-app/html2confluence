e775b8b7bae8902abd9bcd5be12226f3a47b0de2
k1w1 <chris@aha.io>
2015-01-23 16:39:48 -0800

    Adding new table specs. They don't pass yet.

---
+++ spec/complex_tables_spec.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
+require 'html2confluence'
+
+describe HTMLToConfluenceParser, "when running complex tables examples" do
+  
+   it "should handle table with newlines" do
+    html = <<-END
+    <table class="mce-item-table"><tbody><tr><td>As a...</td><td>I would like...</td><td>Because...</td></tr><tr><td><p>Student<br>or</p><p>Teacher</p></td><td>There to be more candy</td><td><p>Candy is:</p><ul><li>Delicious</li><li>Shiny</li><li>Good for my teeth</li></ul></td></tr></tbody></table>
+    END
+
+    markup = <<-END
+|As a...|I would like...|Because...|
+|
+Student
+or
+Teacher|There to be more candy|
+
+Candy is:
+
+* Delicious
+* Shiny
+* Good for my teeth
+|
+    END
+
+    parser = HTMLToConfluenceParser.new
+    parser.feed(html)
+    parser.to_wiki_markup.strip.should include(markup.strip)
+  end
+  
+end
+
+
+
