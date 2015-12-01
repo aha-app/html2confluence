933d957989eb5560d0601c4e3b911b010109f356
k1w1 <k1w1@k1w1.org>
2015-12-01 09:34:37 -0800

    Better handling when closing table cells.

--- lib/html2confluence.rb
+++ lib/html2confluence.rb
@@ -331,8 +331,8 @@ class HTMLToConfluenceParser
     write(cleanup_table_cell(s))
   end
   
-  def cleanup_table_cell(s = [])
-    clean_content = s.join("").strip.gsub(/\n{2,}/, "\n" + '\\\\\\' + "\n")
+  def cleanup_table_cell(s)
+    clean_content = (s || []).join("").strip.gsub(/\n{2,}/, "\n" + '\\\\\\' + "\n")
     # Don't allow a completely empty cell because that will look like a header.
     clean_content = " " if clean_content.empty?
     [clean_content]
